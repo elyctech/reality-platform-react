f2044ae9a4367f952795a48f7afda72d41759320
cyle Ven Dawson <dawsonc623@gmail.com>
2018-03-03 17:24:28 -0600

    Simplify ToolButton component

--- src/tile-master/ToolButton.js
+++ src/tile-master/ToolButton.js
@@ -2,25 +2,11 @@ import React from 'react';
 
 class ToolButton extends React.Component
 {
-  constructor(props)
-  {
-    super(props);
-
-    this.toggleDrawer = this.toggleDrawer.bind(this);
-  }
-
-  toggleDrawer()
-  {
-    this.props.toggleDrawer(
-      this.props.toolId
-    );
-  }
-
   render()
   {
     return (
       <span className = "tool-button"
-            onClick   = {this.toggleDrawer}
+            onClick   = {this.props.onClick}
       >
         {this.props.title}
       </span>
